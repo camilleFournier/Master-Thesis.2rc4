37cfa1658c086552fda55ed8fa6b290e03ef035d
Fournier Camille <cmffournier@gmail.com>
2020-05-14 12:32:24 +0200

    .github

---
+++ .github/parse_json2md.py
@@ -0,0 +1,33 @@
+
+import json
+import sys
+
+def process(f, tex, revision):
+    issues = json.loads(open(f, 'r').read())
+    file = open(tex, 'r').read()
+
+    # language issues
+
+    print("## %s Warnings"%(len(issues["matches"])))
+
+    for m in issues["matches"]:
+        sentence = m["sentence"]
+        rule = m["rule"]["description"]
+        message = m["message"]
+        position = m["offset"]
+        length = m["length"]
+        
+        error = file[position:position + length]
+
+        # estimating line
+        prev = file[:position]
+        n = len(prev.split("\n"))
+
+        #repo = 'https://github.com/Jacarte/autocheck-latex-boilerplate'
+        repo = 'https://github.com/camilleFournier/Master-Thesis.2rc4'
+
+        print(f"- [ ] [*{error}*]({repo}/blob/{revision}/{tex}#L{n}) {message}\n  {rule}")
+        pass
+
+if __name__ == "__main__":
+    process(*sys.argv[1:])
